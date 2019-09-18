9d6ba326fb518a41cbd1d9e0e66c4956a8ef31e6
webkimsora <webkimsora@gmail.com>
2019-09-18 17:32:17 +0900

    스킬수정

--- js/main.js
+++ js/main.js
@@ -36,14 +36,14 @@
 		  }
 		};
 		// bind filter button click
-		$('.filters-button-group').on( 'click', 'button', function() {
+		$('.filterWrap').on( 'click', 'button', function() {
 		  var filterValue = $( this ).attr('data-filter');
 		  // use filterFn if matches value
 		  filterValue = filterFns[ filterValue ] || filterValue;
 		  $thumb.isotope({ filter: filterValue });
 		});
 		// change is-checked class on buttons
-		$('.button-group').each( function( i, buttonGroup ) {
+		$('.btnWrap').each( function( i, buttonGroup ) {
 		  var $buttonGroup = $( buttonGroup );
 		  $buttonGroup.on( 'click', 'button', function() {
 		    $buttonGroup.find('.is-checked').removeClass('is-checked');
