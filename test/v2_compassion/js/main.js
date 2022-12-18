f5748c393b7ef2dd950915a3fc658c5459774131
SORA <webkimsora@gmail.com>
2022-12-19 00:15:26 +0900

    owl

--- test/v2_compassion/js/main.js
+++ test/v2_compassion/js/main.js
@@ -200,12 +200,41 @@
 	//팝업오픈 스크롤방지
 	function scrollOff(){
 		$('html').css('overflow-y','hidden');
+		$('html').css('padding-right',getScrollBarWidth());
 	};
 
 	//팝업오픈 스크롤해제
 	function scrollOn(){
 		$('html').css('overflow-y','auto')
+		$('html').css('padding-right','0');
 	};
+
+	// 스크롤바 넓이 구하기
+	function getScrollBarWidth() {
+		var inner = document.createElement('p');
+		inner.style.width = "100%";
+		inner.style.height = "200px";
+	  
+		var outer = document.createElement('div');
+		outer.style.position = "absolute";
+		outer.style.top = "0px";
+		outer.style.left = "0px";
+		outer.style.visibility = "hidden";
+		outer.style.width = "200px";
+		outer.style.height = "150px";
+		outer.style.overflow = "hidden";
+		outer.appendChild (inner);
+	  
+		document.body.appendChild (outer);
+		var w1 = inner.offsetWidth;
+		outer.style.overflow = 'scroll';
+		var w2 = inner.offsetWidth;
+		if (w1 == w2) w2 = outer.clientWidth;
+	  
+		document.body.removeChild (outer);
+	  
+		return (w1 - w2);
+	  };
 
 /**
  * Youtube API 로드
